990e09f651ca80ea1fd803f4b2422d249c82db75
Lahcen001 <lahcen0000lh@gmail.com>
2023-02-15 23:36:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e273957891ab7b16f2a0c2f137025328'
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     <div className='container'>
+      <div className='top'>
+       <div className="location">Harisson</div>
+         <div className='temp'>
+            <h1>65F</h1> 
+          </div>
+          <div className='description'>
+<p>Clouds</p>
+          </div>
+      </div>
+      <div className='bottom'>d</div>
+     </div>
     </div>
   );
 }
