32f7531b55b923921c5d42da9dcb603b345df36f
Lahcen001 <lahcen0000lh@gmail.com>
2023-02-18 00:10:15 -0600

    add css code and weather api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
 import axios from 'axios'
+import React, {useState} from 'react'
+
+
 
 function App() {
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e273957891ab7b16f2a0c2f137025328'
 
+const [data, setData]= useState({});
+const [location , setLocation] = useState('')
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${location},usa&APPID=e273957891ab7b16f2a0c2f137025328`
+
+
+const searchLocation =(event)=>{
+  if(event.key === 'Enter'){
+    axios.get(url).then((response)=>{
+      setData(response.data)
+      console.log(response.data)
+    })
+  }
+}
 
   return (
-    <div className="app">
+ <div className="app">
+<div className='search'>
+
+
+<input
+value={location}
+onChange={(e)=>setLocation(e.target.value)}
+type="text"
+onKeyPress={(e)=>searchLocation(e)}
+placeholder='Enter Location'
+
+/>
+
+
+</div>
+
+
      <div className='container'>
       <div className='top'>
-       <div className="location">Harisson</div>
+       <div className="location">{location}</div>
          <div className='temp'>
             <h1>65F</h1> 
           </div>
@@ -19,7 +51,27 @@ const url = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99
 <p>Clouds</p>
           </div>
       </div>
-      <div className='bottom'>d</div>
+      <div className='bottom'>
+   
+   <div className='feels'>
+<p className='bold'>65F</p>
+<p >Feels Like</p>
+   </div>
+
+   <div className='humidity'>
+    <p className='bold'>20%</p>
+    <p>Humidity</p>
+   </div>
+
+
+   <div className='wind'>
+  <p className='bold'> 12 MPH</p> 
+    <p>Wind Speed</p>
+   </div>
+
+
+
+      </div>
      </div>
     </div>
   );
