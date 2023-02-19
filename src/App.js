8de155aa5df91ab3adfc807fd23805ada909c413
Lahcen001 <lahcen0000lh@gmail.com>
2023-02-18 23:10:46 -0600

    add loader

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import axios from 'axios'
 import React, {useState} from 'react'
-
+import ProgressBar1 from './ProgressBar1';
 
 
 function App() {
@@ -10,24 +10,28 @@ function App() {
 
 const [data, setData]= useState({});
 const [location , setLocation] = useState('')
+const [loader, setLoader] = useState(false)
 
 const url = `http://api.openweathermap.org/data/2.5/weather?q=${location},usa&APPID=e273957891ab7b16f2a0c2f137025328`
 
 
 const searchLocation =(event)=>{
   if(event.key === 'Enter'){
+    setLoader(true)
     axios.get(url).then((response)=>{
       setData(response.data)
-      console.log(response.data)
+      setLoader(false)
+      
     })
   }
 }
 
   return (
  <div className="app">
+
+ 
 <div className='search'>
-
-
+{loader ? <ProgressBar1/> : <>
 <input
 value={location}
 onChange={(e)=>setLocation(e.target.value)}
@@ -36,36 +40,40 @@ onKeyPress={(e)=>searchLocation(e)}
 placeholder='Enter Location'
 
 />
-
+</>
+  }
 
 </div>
 
 
      <div className='container'>
       <div className='top'>
-       <div className="location">{location}</div>
+       <div className="location">{location}{ location ? (data.sys ?','+ data.sys.country :null) : ''  } </div>
          <div className='temp'>
-            <h1>65F</h1> 
+
+
+            <h1>{data.main ? data.main.temp :null  }°F</h1> 
           </div>
           <div className='description'>
-<p>Clouds</p>
+<p>{data. weather ? data.weather[0].description :null  }
+</p>
           </div>
       </div>
       <div className='bottom'>
    
    <div className='feels'>
-<p className='bold'>65F</p>
+<p className='bold'>   {data.main ? data.main.feels_like:null  }°F</p>
 <p >Feels Like</p>
    </div>
 
    <div className='humidity'>
-    <p className='bold'>20%</p>
+    <p className='bold'>{data.main ? data.main.humidity:null  }% </p>
     <p>Humidity</p>
    </div>
 
 
    <div className='wind'>
-  <p className='bold'> 12 MPH</p> 
+  <p className='bold'>{data.wind ? data.wind.speed:null  }MPH</p> 
     <p>Wind Speed</p>
    </div>
 
@@ -73,6 +81,8 @@ placeholder='Enter Location'
 
       </div>
      </div>
+
+    
     </div>
   );
 }
---
+++ src/ProgressBar1.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import {ProgressBar} from  'react-loader-spinner'
+
+export default function ProgressBar1() {
+  return (
+    <div>
+
+<ProgressBar
+  height="80"
+  width="80"
+  ariaLabel="progress-bar-loading"
+  wrapperStyle={{}}
+  wrapperClass="progress-bar-wrapper"
+  borderColor = '#F4442E'
+  barColor = '#51E5FF'
+/>
+
+
+    </div>
+  )
+}
